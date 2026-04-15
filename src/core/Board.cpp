5a7a522c3313b3a247738da77f9e4b430b62df30
viuch3 <viuch3ia@gmail.com>
2026-04-14 19:41:46 -0500

    Se crean los metodos getIndex y getCoord

--- src/core/Board.cpp
+++ src/core/Board.cpp
@@ -1,11 +1,15 @@
 #include "Board.h"
-#include <iostream>
 
-int main() {
-  Board b = Board();
-  for (int i = 0; i < b.tablero.size(); i++) {
-    std::cout << b.tablero[i] << "\n";
-  }
+int Board::getIndex(std::string coord) {
+  return 8 * (int(coord[1]) - int('0') - 1) + int(coord[0]) - 65;
 }
 
+std::string Board::getCoord(int index) {
+  char c = char(65 + (index % 8));
+  int f = (index / 8) + 1;
 
+  std::string coord;
+  coord += c;
+  coord += std::to_string(f);
+  return coord;
+}
--- src/core/Board.h
+++ src/core/Board.h
@@ -1,7 +1,11 @@
 #pragma once
 #include <array>
+#include <string>
 
 struct Board {
   std::array<int, 64> tablero{};
   Board () {}
+  
+  int getIndex(std::string coord);
+  std::string getCoord(int index);
 };
